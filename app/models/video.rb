af20ce2b812c87e6ba4d67f32f7329bb28442771
vishalsingh <vishal.singh030@gmail.com>
2017-07-29 16:29:27 +0530

    d

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,14 +3,22 @@ class Video < ApplicationRecord
 	belongs_to :user
     validates :title, presence: true
     validates :description, presence: true
-    validates :archive_video, presence: true
-	has_attached_file :archive_video, :styles => {
-    :medium => { :geometry => "640x480", :format => 'mp4' },
-    :cuadro1 => { :geometry => "640x480#", :format => 'jpg', :time => 1 },
-    :cuadro2 => { :geometry => "640x480#", :format => 'jpg', :time => 30 },
-    :cuadro3 => { :geometry => "640x480#", :format => 'jpg', :time => 50 },
-    :cuadro4 => { :geometry => "640x480#", :format => 'jpg', :time => 100 }
-  }, :processors => [:transcoder]
+     has_attached_file :archive_video, styles: {
+        :medium => {
+          :geometry => "640x480",
+          :format => 'mp4'
+        },
+        :thumb => { :geometry => "160x120", :format => 'jpeg', :time => 10}
+    }, :processors => [:transcoder]
+    validates_attachment_content_type :archive_video, content_type: /\Avideo\/.*\Z/
+ #    validates :archive_video, presence: true
+	# has_attached_file :archive_video, :styles => {
+ #    :medium => { :geometry => "640x480", :format => 'mp4' },
+ #    :cuadro1 => { :geometry => "640x480#", :format => 'jpg', :time => 1 },
+ #    :cuadro2 => { :geometry => "640x480#", :format => 'jpg', :time => 30 },
+ #    :cuadro3 => { :geometry => "640x480#", :format => 'jpg', :time => 50 },
+ #    :cuadro4 => { :geometry => "640x480#", :format => 'jpg', :time => 100 }
+ #  }, :processors => [:transcoder]
 
 
  # has_attached_file :archive_video, styles: {
@@ -18,6 +26,6 @@ class Video < ApplicationRecord
  #        :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 },
  #        :cuadro1 => { :geometry => "910x521", :format => 'jpg', :time => 1 }
  #    }, :processors => [ :ffmpeg ]
-    validates_attachment_content_type :archive_video, content_type: /\Avideo\/.*\Z/
+    #validates_attachment_content_type :archive_video, content_type: /\Avideo\/.*\Z/
  #    validates_attachment_content_type :archive_video, :content_type => ["video/mp4", "video.mov", "video/mpeg","video/mpeg4"]
 end
